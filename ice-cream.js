38e792c3eff397089040d903229f2c6c1dfd2355
Allen <headhuntar@gmail.com>
2020-02-17 16:52:40 -0800

    looping through arrays

--- ice-cream.js
+++ ice-cream.js
@@ -1,2 +1,7 @@
 const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
 console.log(iceCreamFlavours);
+iceCreamFlavours.push('root beer')
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0])
+console.log(iceCreamFlavours[iceCreamFlavours.length])
+console.log(iceCreamFlavours.length)
